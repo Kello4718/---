138db3e9018e4a7df8323c995faa9a10b8ece64a
Kello4718 <Dolorem.scire@gmail.com>
2023-03-22 21:16:52 +0300

    add update

--- js/script.js
+++ js/script.js
@@ -2,8 +2,24 @@ window.addEventListener('scroll', () => {
 	document.documentElement.style.setProperty('--scrollTop', `${this.scrollY}px`);
 })
 
-gsap.registerPlugin(ScrollTrigger, ScrollSmoother)
-ScrollSmoother.create({
+gsap.registerPlugin(ScrollTrigger, ScrollSmoother, SplitText)
+const smoother = ScrollSmoother.create({
 	wrapper: '.smooth-wrapper',
-	content: '.smooth-content'
+	content: '.smooth-content',
+	smooth: 1.2,
+	effects: true,
 })
+
+const title = new SplitText('.preview__pretitle', {type: 'words, chars'});
+const subtitle = new SplitText('.preview__title', {type: 'words, chars'});
+
+const titleChars = title.chars;
+const subtitleChars = subtitle.chars;
+
+titleChars.forEach((char, i) => {
+	smoother.effects(char, { speed: 1, lag: (i + 1) * 0.3 });
+});
+
+subtitleChars.forEach((char, i) => {
+	smoother.effects(char, { speed: 1, lag: (i + 1) * 0.3 });
+});
